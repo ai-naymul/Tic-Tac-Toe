489850fe1a2bc540c51919b316728f3dd77e1460
Naymul Islam <naymul504@gmail.com>
2023-10-24 14:23:34 +0600

    print function of the board and initialize the state variable

---
+++ main.py
@@ -0,0 +1,11 @@
+xState = [0,0,0,0,0,0,0,0]
+oState = [0,0,0,0,0,0,0,0]
+
+def printboard():
+  print(f"0 | 1 | 2")
+  print(f"----------")
+  print(f"3 | 4 | 5")
+  print(f"----------")
+  print(f"6 | 7 | 8")
+
+printboard()
